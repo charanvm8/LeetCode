599500e8c6a26f3e1c9abb61360ae982d3265a7e
Charan Mareedu <charanvm8@gmail.com>
2018-04-09 20:24:48 -0400

    arrays

--- LeetCode/src/arrays/FindAllNumbersDisappearedinanArray.java
+++ LeetCode/src/arrays/FindAllNumbersDisappearedinanArray.java
@@ -6,19 +6,16 @@ import java.util.List;
 public class FindAllNumbersDisappearedinanArray {
 	public static List<Integer> findDisappearedNumbers(int[] nums) {
 		List<Integer> resultList=new LinkedList<Integer>();
-		if(nums==null || nums.length==0) {
+		
+        if(nums==null || nums.length==0) {
 			return resultList;
 		}
 		for(int i = 0; i < nums.length; i++) {
             int val = Math.abs(nums[i]) - 1;
-            System.out.println(val);
             if(nums[val] > 0) {
-                nums[val] = -nums[val];
+                nums[val] =-nums[val];
             }
         }
-		/*for(int i:nums) {
-			System.out.println(i);
-		}*/
 		for(int i = 0; i < nums.length; i++) {
             if(nums[i] > 0) {
             	resultList.add(i+1);
@@ -30,8 +27,8 @@ public class FindAllNumbersDisappearedinanArray {
 		// TODO Auto-generated method stub
 		int[] nums= {4,3,2,7,8,2,3,1};
 		List<Integer> numList=findDisappearedNumbers(nums);
-		/*for(Integer i:numList) {
+		for(Integer i:numList) {
 			System.out.println(i);
-		}*/
+		}
 	}
 }
